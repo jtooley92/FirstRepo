8d9e197e5c3b79795486bf5bce094afaba046585
jtooley92 <jtooley92@gmail.com>
2020-09-03 19:36:35 -0500

    method practice

---
+++ helloWorld/src/main/java/com/mylesson/helloworld/MethodPractice.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mylesson.helloworld;
+
+/**
+ *
+ * @author Jtooleyful
+ */
+public class MethodPractice {
+    public static void main(String[] args) {
+        
+    } 
+    
+    public static int randomVariable(int a, int b) {
+        return a;
+    }
+    
+    public static String colors(String yellow) {
+        return yellow;
+    }
+    
+    public static void nothing() {
+        
+    }
+}
